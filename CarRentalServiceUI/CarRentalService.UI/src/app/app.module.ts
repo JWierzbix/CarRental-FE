1d8bbb57c2d38123218b58ac094453dd45cd0186
Tomasz-Budny <s208246@sggw.edu.pl>
2023-01-06 15:43:25 +0100

    add about us component

--- CarRentalServiceUI/CarRentalService.UI/src/app/app.module.ts
+++ CarRentalServiceUI/CarRentalService.UI/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { BackgroundS1Component } from './home/background-s1/background-s1.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './home/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     BackgroundS1Component,
-    HomeComponent
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
