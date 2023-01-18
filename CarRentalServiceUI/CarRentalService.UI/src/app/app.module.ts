abc4b89c32b6471324de4d3379e1d07bc7cf0d5d
Tomasz-Budny <s208246@sggw.edu.pl>
2023-01-18 18:56:35 +0100

    add cars component, configure routing

--- CarRentalServiceUI/CarRentalService.UI/src/app/app.module.ts
+++ CarRentalServiceUI/CarRentalService.UI/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { BackgroundS1Component } from './home/background-s1/background-s1.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './home/about-us/about-us.component';
+import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AboutUsComponent } from './home/about-us/about-us.component';
     NavigationComponent,
     BackgroundS1Component,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
