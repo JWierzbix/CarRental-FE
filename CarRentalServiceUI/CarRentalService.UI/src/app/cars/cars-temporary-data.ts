507cfc0280f50a045346f6dd1fa9b0d05a52a679
Tomasz-Budny <s208246@sggw.edu.pl>
2023-01-19 18:22:48 +0100

    add car service and car model

---
+++ CarRentalServiceUI/CarRentalService.UI/src/app/cars/cars-temporary-data.ts
@@ -0,0 +1,17 @@
+import { Car } from "./car.model";
+
+export const cars: Car[] = [
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+    new Car('M8 Competition', 'BMW', 1100, 3.0, 220, 'All wheel', 2.3),
+]
--- CarRentalServiceUI/CarRentalService.UI/src/app/cars/cars.component.ts
+++ CarRentalServiceUI/CarRentalService.UI/src/app/cars/cars.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from './car.model';
+import { CarService } from './car.service';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.scss']
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit {
+  carsObservable: Observable<Car[]>;
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit(): void {
+    this.carsObservable = this.carService.getAllCars()
+
+    this.carsObservable.subscribe((cars) => {
+      console.log(cars);
+    });
+  }
 
 }
