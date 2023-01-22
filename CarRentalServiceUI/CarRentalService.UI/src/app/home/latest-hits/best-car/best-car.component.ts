ba6210494ce0b0d48b59a12b8c80437e7cd869a5
Tomasz-Budny <s208246@sggw.edu.pl>
2023-01-22 14:21:14 +0100

    add best-car component

---
+++ CarRentalServiceUI/CarRentalService.UI/src/app/home/latest-hits/best-car/best-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-car',
+  templateUrl: './best-car.component.html',
+  styleUrls: ['./best-car.component.scss']
+})
+export class BestCarComponent {
+
+}
