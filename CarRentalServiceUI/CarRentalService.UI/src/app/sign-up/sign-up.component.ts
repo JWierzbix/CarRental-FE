f28233aa753d0a3067c2d594ba2080618acb1e54
Tomasz-Budny <s208246@sggw.edu.pl>
2023-01-23 00:17:11 +0100

    add form

--- CarRentalServiceUI/CarRentalService.UI/src/app/sign-up/sign-up.component.ts
+++ CarRentalServiceUI/CarRentalService.UI/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  @ViewChild('form') form: NgForm
 
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    console.log(this.form.value);
+    //this.authService.register()
+  }
 }
